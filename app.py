10c622b91a1954cf849101b7e7ca96d76aac4df5
flairmix <d9067246810@gmail.com>
2025-04-04 17:10:23 +0300

    folders

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # app.py
 from litestar import Litestar
 from litestar.openapi import OpenAPIConfig
-from oks_routes import oks_router
-from version_router import version_router
+from routers.oks_routes import oks_router
+from routers.version_router import version_router
 from dependencies import provide_session
 
 openapi_config = OpenAPIConfig(title="KSI OKS API", version="1.0.0")
